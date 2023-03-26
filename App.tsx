aaa172bcd5cc2b7d4294f14ebefa1cea44e8e1c0
InesMasllorens <ines.maria.masllorens@estudiantat.upc.edu>
2023-03-26 15:59:25 +0200

    Register done
    He hecho el Register.

--- App.tsx
+++ App.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import MainContainer from './components/Containers/MainContainer';
 import LoginScreen from './components/Screens/LoginScreen';
 import RegisterScreen from './components/Screens/RegisterScreen';
+// import { StyleSheet, Text, View, TextInput, Alert, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  let page = 'login';
+
+
+
   return (
-    <LoginScreen></LoginScreen>
+    <>
+   
+       {/* <LoginScreen></LoginScreen> */}
+      
+      
+      <RegisterScreen></RegisterScreen>
+      
+     
+    </>
   );
 }
--- components/Screens/RegisterScreen.tsx
+++ components/Screens/RegisterScreen.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar'; //esto es lo que tenemos arriba del móvil, la hora la carga etc 
 import React from 'react';
 import { StyleSheet, Text, View, TextInput, Alert, TouchableOpacity } from 'react-native';
-import ButtonGradient from '../Buttons/ButtonGradient';
+import ButtonGradientRegister from '../Buttons/ButtonGradientRegister';
 import MainContainer from '../Containers/MainContainer';
 import SubTitle from '../Texts/Subtitle';
 import Title from '../Texts/Title';
@@ -12,7 +12,34 @@ export default function RegisterScreen() {
   
   return (
     <MainContainer>
-      <Title>Register</Title>      
+    <Title>Register</Title>
+    <SubTitle>Register to have a new account</SubTitle>
+    <StyledTextInputs placeholder='Email'></StyledTextInputs>
+    <StyledTextInputs placeholder='Password'></StyledTextInputs>
+    <StyledTextInputs placeholder='Phone Number'></StyledTextInputs>
+    <StyledTextInputs placeholder='Country'></StyledTextInputs>
+  
+    <ButtonGradientRegister onPress={() => {
+            Alert.alert(
+            'Mensaje',
+            'Has intentado iniciar sesión',
+            [{ text: 'OK', onPress: () => console.log('OK Pressed') }],
+            { cancelable: false }
+            );
+        }} />
+
+    <TouchableOpacity onPress={() => {
+        Alert.alert(
+            'Mensaje',
+            'Has intentado registrarte',
+            [{ text: 'OK', onPress: () => console.log('OK Pressed') }],
+            { cancelable: false }
+        );
+    }}>
+       
+    </TouchableOpacity>
+    <StatusBar style="auto" /> 
     </MainContainer>
+
   );
 }
