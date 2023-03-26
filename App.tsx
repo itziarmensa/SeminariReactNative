b13d1a5f685355a4a6d99ed54299c02f63406166
Itziar Mensa <itziar.mensa@estudiantat.upc.edu>
2023-03-26 12:06:55 +0200

    Arreglos

--- App.tsx
+++ App.tsx
@@ -3,15 +3,9 @@ import { StyleSheet, Text, View } from 'react-native';
 import MainContainer from './components/Containers/MainContainer';
 import LoginScreen from './components/Screens/LoginScreen';
 import RegisterScreen from './components/Screens/RegisterScreen';
-import { createStackNavigator } from '@react-navigation/stack';
-
-const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <Stack.Navigator initialRouteName="Login">
-      <Stack.Screen name="Login" component={LoginScreen} />
-      <Stack.Screen name="Home" component={RegisterScreen} />
-    </Stack.Navigator>
+    <LoginScreen></LoginScreen>
   );
 }
